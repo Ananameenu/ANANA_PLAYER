24e8303029263a4dcba7a76033391c2576ca6456
unknown <meenakshi76764@gmail.com>
2022-01-17 00:10:22 +0530

    first cls clear
    :

---
+++ data.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+      name: 'song 1',
+      path: 'music/Song 1.mp3',
+      artist: 'artist 1',
+      cover: 'cover/cover 1.jpg'
+    },
+    {
+       name: 'song 2',
+       path: 'music/Song 2.mp3',
+       artist: 'artist 2',
+       cover: 'cover/cover 2.jpg'
+    },
+    {
+        name: 'song3',
+        path: 'music/Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'cover/cover 3.jpg'  
+       },
+]
